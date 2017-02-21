eb4ea847d0d096b2e28241c22151d56f4e9f9b4f
Fazar Rahman <fazar.rahman@gmail.com>
2017-02-21 16:34:35 +0700

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,7 @@ import groovy.json.JsonSlurperClassic
 node {
     env.AWS_DEFAULT_REGION = 'ap-southeast-1'
 	
-	def applicationName = 'fazarrahman' //change me
+	def applicationName = 'agungbd' //change me
 	def deploymentGroupName = 'fazarrahman' // change me
 	def s3BucketName = 'deployment-cdc'
 	
